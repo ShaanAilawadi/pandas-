04693f213383f6c0ff398ebee8cb87c21deecd8b
ShaanAilawadi <ailawadis04@gmail.com>
2021-09-12 00:51:04 -0400

    Update Intro.py

--- Data/Intro.py
+++ Data/Intro.py
@@ -1,3 +1,9 @@
 import pandas as pd
-df = pd.read_csv("survey_results_public.csv")
-df()
+df = pd.read_csv('survey_results_public.csv')
+schema_df = pd.read_csv('survey_results_schema.csv')
+pd.set_option('display.max_columns', 48)
+pd.set_option('display.max_rows', 48)
+df.head()
+schema_df
+df['Ethnicity']
+df.loc[0:2, 'Gender':'Ethnicity']
