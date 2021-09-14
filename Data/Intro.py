3f9bf6ac886cd38f5413e399f1bdb68df80fc7fe
ShaanAilawadi <ailawadis04@gmail.com>
2021-09-13 21:54:04 -0400

    Update Intro.py

--- Data/Intro.py
+++ Data/Intro.py
@@ -4,5 +4,5 @@ schema_df = pd.read_csv('survey_results_schema.csv', index_col='Column')
 pd.set_option('display.max_columns', 48)
 pd.set_option('display.max_rows', 48)
 df.head()
-schema_df.loc['Gender']
-schema_df.sort_index()
+schema_df.loc['Ethnicity']
+schema_df.sort_index(inplace=True)
