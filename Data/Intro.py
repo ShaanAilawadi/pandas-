eff8167eb4e30e967126d2aac4e34dfa2542970b
ShaanAilawadi <ailawadis04@gmail.com>
2021-09-18 21:42:07 -0400

    Update Intro.py

--- Data/Intro.py
+++ Data/Intro.py
@@ -1,8 +1,9 @@
 import pandas as pd
 df = pd.read_csv('survey_results_public.csv', index_col='ResponseId')
-schema_df = pd.read_csv('survey_results_schema.csv', index_col='Column')
+# schema_df = pd.read_csv('survey_results_schema.csv', index_col='Column')
 pd.set_option('display.max_columns', 48)
 pd.set_option('display.max_rows', 48)
 df.head()
-schema_df.loc['Ethnicity']
-schema_df.sort_index(inplace=True)
+high_salary = (df['ConvertedCompYearly'] > 70000)
+filt = df['LanguageHaveWorkedWith'].str.contains('Python', na=False)
+df.loc[filt, 'LanguageHaveWorkedWith']
