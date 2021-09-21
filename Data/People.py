d553c49f497aa4f25dc0aa9faa6c4d164b902c91
ShaanAilawadi <ailawadis04@gmail.com>
2021-09-21 00:51:09 -0400

    Update People.py

--- Data/People.py
+++ Data/People.py
@@ -20,5 +20,12 @@ import pandas as pd
 
 df = pd.DataFrame(people)
 df
-filt = (df['last'] == 'Schafer') & (df['first'] == 'John')
-df.loc[~filt, 'email']
+df.columns = ['first_name', 'last_name', 'email']
+df.columns = [x.lower() for x in df.columns]
+df.rename(columns={'first_name': 'first', 'last_name': 'last'}, inplace=True)
+df.loc[2] = ['John', 'Smith', 'JohnDoe@email.com']
+df.loc[2, ['last', 'email']] = ['Doe', 'JohnDoe@email.com']
+df.loc[2, 'last'] = 'Smith'
+filt = (df['email'] == 'JohnDoe@email.com')
+df.loc[filt, 'last'] = 'Smith'
+df['email'] = df['email']. str.lower()
