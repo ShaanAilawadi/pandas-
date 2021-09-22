baca3160b7139214d9e10bac265f2ec8ff5607bf
ShaanAilawadi <ailawadis04@gmail.com>
2021-09-22 03:38:08 -0400

    Update People.py

--- Data/People.py
+++ Data/People.py
@@ -29,3 +29,18 @@ df.loc[2, 'last'] = 'Smith'
 filt = (df['email'] == 'JohnDoe@email.com')
 df.loc[filt, 'last'] = 'Smith'
 df['email'] = df['email']. str.lower()
+df['email'].apply(len)
+
+
+def update_email(email):
+    return email.upper()
+
+
+df['email'] = df['email'].apply(update_email)
+df['email'] = df['email'].apply(lambda x: x.lower())
+df.apply(len, axis='columns')
+(len)(df['email'])
+df.apply(pd.Series.min)
+df.apply(lambda x: x.min())
+df.applymap(len)
+df.applymap(str.lower)
