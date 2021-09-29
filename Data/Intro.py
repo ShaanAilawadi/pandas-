48c19fd30b69fe14e17da5f71749bdc664432364
ShaanAilawadi <ailawadis04@gmail.com>
2021-09-29 06:11:20 -0400

    Update Intro.py

--- Data/Intro.py
+++ Data/Intro.py
@@ -5,5 +5,7 @@ pd.set_option('display.max_columns', 48)
 pd.set_option('display.max_rows', 48)
 df.head()
 high_salary = (df['ConvertedCompYearly'] > 70000)
-filt = df['LanguageHaveWorkedWith'].str.contains('Python', na=False)
-df.loc[filt, 'LanguageHaveWorkedWith']
+#filt = df['LanguageHaveWorkedWith'].str.contains('Python', na=False)
+#df.loc[filt, 'LanguageHaveWorkedWith']
+df.rename(columns={'ConvertedCompYearly': 'Salary'}, inplace=True)
+df['Trans'] = df['Trans'].map({'Yes': True, 'No': False})
