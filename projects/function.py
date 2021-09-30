9c685076102991b81829ae6f8e8d4eb922954359
ShaanAilawadi <ailawadis04@gmail.com>
2021-09-29 21:24:17 -0400

    Create function.py

---
+++ projects/function.py
@@ -0,0 +1,12 @@
+def equation(x):
+    y = 5 * x + 3
+
+    return y
+
+
+for i in range(1, 101):
+    mrs_long_wants = i
+
+    naveens_answer = equation(mrs_long_wants)
+    print(f"Mrs. Long wants: {mrs_long_wants}")
+    print(f"Naveen's Answer: {naveens_answer}")
