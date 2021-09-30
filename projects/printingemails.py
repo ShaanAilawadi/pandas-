02578a3fee8e5f8567a1c75034ad27466aa11569
ShaanAilawadi <ailawadis04@gmail.com>
2021-09-29 21:24:12 -0400

    Update printingemails.py

--- projects/printingemails.py
+++ projects/printingemails.py
@@ -1,7 +1,12 @@
 import pandas as pd
-df = pd.read_csv('email.csv')
 
 
-def email(filename):
+def print_emails(filename):
+    df = pd.read_csv(filename)
+
     for index, row in df.iterrows():
-        print(index, row['email'])
+        print(f"{row['company']}'s email: \n{row['email']}\n")
+
+
+input_file = input('Filename: ')
+print_emails(input_file)
